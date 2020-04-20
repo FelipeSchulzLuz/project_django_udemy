9fc49bc63b7b46794081070e74c8e0dfa89a5c7a
Felipe Schulz <schulz.neo@.gmail.com>
2020-04-20 12:57:38 -0300

    v_3

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('',views.home, name="home")
+     path('', views.home, name="home"),
+     path('post/<int:pk>/', views.detalhe_post, name="detalhe_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,16 @@ from blog.models import Postagem
 
 # Create your views here.
 
-def home(request):
-    postagens = Postagem.objects.all().order_by('-data_criacao')
 
-    return render(request, 'home.html', {'postagens':postagens})
-    
+def home(request):
+    postagens = Postagem.objects.all().order_by("-data_criacao")
+    return render(request, 'home.html', {'postagens': postagens})
+
+
+def detalhe_post(request, pk):
+    postagem = Postagem.objects.get(pk=pk)
+    return render(request, 'detalhe_postagem.html', {'postagem': postagem})
+
+
+
+
