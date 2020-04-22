38384ad2d7ebbdb35bfe393f00e26074c13653a8
Felipe Schulz <schulz.neo@.gmail.com>
2020-04-22 18:54:11 -0300

    v_4

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from blog.models import Postagem
+from .forms import PostForm
+from django.utils import timezone
+from django.shortcuts import redirect, get_object_or_404
+
 
 # Create your views here.
 
@@ -14,5 +18,38 @@ def detalhe_post(request, pk):
     return render(request, 'detalhe_postagem.html', {'postagem': postagem})
 
 
+
+def adicionar_postagem(request):
+    #Quando para salvar
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            postagem = form.save(commit=False)
+            postagem.autor = request.user
+            postagem.data_publicacao = timezone.now()
+            postagem.save()
+            return redirect('home')
+    else:
+
+        form = PostForm()
+
+    return render(request, 'editar_postagem.html', {'form':form})
+
+
+def edit_postagem(request, pk):
+    postagem = get_object_or_404(Postagem, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=postagem)
+        if form.is_valid():
+            postagem = form.save(commit=False)
+            postagem.autor = request.user
+            postagem.data_publicacao = timezone.now()
+            postagem.save()
+            return redirect('home')
+    else:
+
+        form = PostForm(instance=postagem)
+
+    return render(request, 'editar_postagem.html', {'form':form})
 
 
